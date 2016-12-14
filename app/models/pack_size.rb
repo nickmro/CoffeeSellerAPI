aa173961d7451567ef393a881d6e2ef8c7180ba2
Nick Mrozowski <nickmro@gmail.com>
2016-12-14 01:09:43 -0800

    Add Pack Size model attributes

--- app/models/pack_size.rb
+++ app/models/pack_size.rb
@@ -1,3 +1,12 @@
 class PackSize
   include Mongoid::Document
+
+  ## Attributes
+  field :name, type: String
+  field :quantity, type: Integer
+
+  ## Validations
+  validates :name, presence: true
+  validates :quantity, presence: true
+
 end
--- test/models/pack_size_test.rb
+++ test/models/pack_size_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PackSizeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "A Pack Size must have a name" do
+    pack_size = PackSize.new
+    pack_size.save
+    assert pack_size.errors.keys.include?(:name)
+  end
+
+  test "A Pack Size must have a quantity" do
+    pack_size = PackSize.new
+    pack_size.save
+    assert pack_size.errors.keys.include?(:quantity)
+  end
+
 end
