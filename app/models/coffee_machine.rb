e519398eff5c330d2e5157c6027162a815551713
Nick Mrozowski <nickmro@gmail.com>
2016-12-15 01:10:14 -0800

    Remove unneeded code

--- app/models/coffee_machine.rb
+++ app/models/coffee_machine.rb
@@ -5,7 +5,6 @@ class CoffeeMachine < Product
   field :water_line_compatible, type: Boolean
 
   ## Relations
-  has_many :coffee_pods, inverse_of: :coffee_machine
   belongs_to :pod_size
 
   ## Validations
